7510ab0f9272a3ec9f6a435432dbb65b8d21243a
itggot-jon-mcgarvie <60005848+itggot-jon-mcgarvie@users.noreply.github.com>
2020-01-30 10:43:30 +0100

    Fortsatte på login funktion

--- app.rb
+++ app.rb
@@ -5,17 +5,21 @@ require "bcrypt"
 
 enable :sessions
 
+def set_error(error_message)
+    session[:error] = error_message
+end
+
 get("/") do
     slim(:start)
 end
 
 post('/register') do
-    db = SQLite3::Database.new("db/tabdatabase.db.sql")
+    db = SQLite3::Database.new("db/tabdatabase.db")
     db.results_as_hash = true
     username = params[:register_username]
     password = params[:register_password]
     
-    result = db.execute("SELECT id FROM User WHERE username = ?", username)
+    result = db.execute("SELECT user_id FROM User WHERE username = ?", username)
 
     if result.empty?
         password_digest = BCrypt::Password.create(password)
@@ -23,4 +27,39 @@ post('/register') do
         redirect('/register_confirmation')
     end
     redirect('/register_confirmation')
+end
+
+get('/register_confirmation') do
+    db = SQLite3::Database.new('db/tabdatabase.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM User")
+    slim(:register_confirmation, locals:{result:result})
+end
+
+post('/login') do
+    
+    username = params[:username]
+    session[:username] = username
+    password = params[:password]
+    db = SQLite3::Database.new("db/database.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT user_id,password FROM User WHERE username = ?", username)
+    if result.empty?
+        set_error("Invalid username or password")
+        redirect('/error')
+    end
+
+    user_id = result.first["id"]
+    password_digest = result.first["password_digest"]
+    if BCrypt::Password.new(password_digest) == password
+        session[:user_id] = user_id
+        redirect('/login_sida')
+    else
+        set_error("Invalid username or password")
+        redirect('/error')
+    end
+end
+
+get('/error') do
+    slim(:error)
 end
